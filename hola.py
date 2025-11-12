f5c2964d8528e04271b01917da4b00282eeff10d
Eddie <nagashdevmed@gmail.com>
2025-11-11 21:11:25 -0500

    Inicia prueba con git, hola.py

---
+++ hola.py
@@ -0,0 +1,7 @@
+# hola.py
+# Autor: Edison Alexis Medrano
+# Descripcion: Scrip Básico que saluda al usuario
+# Unisalamanca - Ing. de Sistemas de Información
+
+nombre = input("¿como te llamas? ")
+print(f"Hola, {nombre}! Bienvenido a tu primero proyecto con Git y Python")
