04f5f7378936202c6e7f3e40a1022422bad656f0
nagashDev <nagashdevmed@gmail.com>
2026-03-16 18:38:37 -0500

    Edison prueba desde marzo

--- hola.py
+++ hola.py
@@ -5,3 +5,4 @@
 nombre = input("¿como te llamas? ")
 print(f"Hola, {nombre}! Bienvenido a tu primero proyecto con Git y Python")
 print(f"Tu nombre tiene {len(nombre)} letras.")
+#esta es una prueba de Edison Medrano
